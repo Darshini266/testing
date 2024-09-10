cfcec8bcbd3829875ed5b10d48b6d507f887e971
Darshini266 <darshini.n@cittabase.com>
2024-09-10 14:54:13 +0530

    Add files via upload

---
+++ customer.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE TABLE CUSTOMER (
+Index INT,
+Customer_id Varchar(100),
+Name Varchar(100),
+Phone Varchar(100),
+Email Varchar(100),
+Salary int,
+Tgt_Create_Dt Timestamp,
+Tgt_Update_Dt Timestamp,
+File_Name Varchar(100)
+)
+
+;
